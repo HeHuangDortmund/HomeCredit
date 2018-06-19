773dcab19833ff9433e76ab27fb98ce649e7746e
Fang Duan <fang.duan.hu@gmail.com>
2018-06-19 17:37:40 +0200

    calculate percentage of the occurance of STATUS with respect to each SK_ID_BUREAU instead of pure SUM

--- data/readdata_bureau_balance.R
+++ data/readdata_bureau_balance.R
@@ -2,13 +2,13 @@
 readData = function(){
   library(rprojroot)
   library(data.table)
-  requireNamespace("lubridate")
   root = find_root(is_git_root)
   setwd(root)
   
   bureau_balance = fread("../data/bureau_balance.csv", na.strings = "")
   tmp = bureau_balance[,.N, by = list(SK_ID_BUREAU,STATUS)]
-  wideData = dcast(tmp, SK_ID_BUREAU ~ STATUS, fill = 0)
+  tmp = tmp[, percentage := N/sum(N), by = SK_ID_BUREAU]
+  wideData = dcast(tmp, SK_ID_BUREAU ~ STATUS, value.var = "percentage", fill = 0)
   names(wideData) = c("SK_ID_BUREAU", 
                       "BUREAU_STATUS_0", 
                       "BUREAU_STATUS_1", 
