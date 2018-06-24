51270c049f51dfff5796faddabc8fc08b1b9983d
Fang Duan <fang.duan.hu@gmail.com>
2018-06-24 22:21:35 +0200

    read data/data cleaning for credit_card_balance & installments_payments

--- data/readdata_credit_card_balance.R
+++ data/readdata_credit_card_balance.R
@@ -1,12 +1,108 @@
-
-readData = function(){
+readData = function(exploratory = 0){
   library(rprojroot)
   library(data.table)
-  requireNamespace("lubridate")
   root = find_root(is_git_root)
   setwd(root)
+  ########################################################################################
+  # Problem: fill NAs, e.g., CNT_INSTALMENT_MATURE_CUM, AMT_INST_MIN_REGULARITY, AMT_PAYMENT_CURRENT
+  ########################################################################################
+  makeplot <- function(var, data, type){
+    pdf(file.path(data,"_",var,".pdf",fsep = ""), 
+        width = 14, 
+        height = 12)
+    par(cex.lab=1.5,cex.axis=1.5,mar=c(8,5,2,2) + 0.1,lwd=2)
+    if (type == "barplot"){
+      eval(parse(text = file.path("barplot(",data,"[,.N,by =", 
+                                  var,
+                                  "]$N,names.arg  = ",data,"[,.N,by =",
+                                  var,
+                                  "]$",
+                                  var,
+                                  ",las = 2)",
+                                  fsep = "")))
+    } else if (type == "density") {
+      eval(parse(text = file.path("plot(density(na.omit(",data,"$", 
+                                  var,
+                                  ")))",
+                                  fsep = "")))
+    }
+    dev.off()
+  }
   
   credit_card_balance = fread("../data/credit_card_balance.csv", na.strings = "")
+  varCategory = names(credit_card_balance)[sapply(credit_card_balance, function(x) is.character(x))]
+  varNumeric = setdiff(names(credit_card_balance),varCategory)
+  sort(sapply(credit_card_balance,function(x) sum(is.na(x))))
+  
+  credit_card_balance$AMT_DRAWINGS_ATM_CURRENT[is.na(credit_card_balance$AMT_DRAWINGS_ATM_CURRENT)] = 0
+  credit_card_balance$AMT_DRAWINGS_OTHER_CURRENT[is.na(credit_card_balance$AMT_DRAWINGS_OTHER_CURRENT)] = 0
+  credit_card_balance$AMT_DRAWINGS_POS_CURRENT[is.na(credit_card_balance$AMT_DRAWINGS_POS_CURRENT)] = 0
+  credit_card_balance$CNT_DRAWINGS_ATM_CURRENT[is.na(credit_card_balance$CNT_DRAWINGS_ATM_CURRENT)] = 0
+  credit_card_balance$CNT_DRAWINGS_OTHER_CURRENT[is.na(credit_card_balance$CNT_DRAWINGS_OTHER_CURRENT)] = 0
+  credit_card_balance$CNT_DRAWINGS_POS_CURRENT[is.na(credit_card_balance$CNT_DRAWINGS_POS_CURRENT)] = 0
+  credit_card_balance$AMT_INST_MIN_REGULARITY[is.na(credit_card_balance$AMT_INST_MIN_REGULARITY)] = 0
+  credit_card_balance$AMT_PAYMENT_CURRENT[is.na(credit_card_balance$AMT_PAYMENT_CURRENT)] = 0
+  credit_card_balance$CNT_INSTALMENT_MATURE_CUM[is.na(credit_card_balance$CNT_INSTALMENT_MATURE_CUM)] = 0 # 暂时补0,需merge installments 信息
+  
+  temp = credit_card_balance[,.(N = .N,
+                                AMT_BALANCE_MEAN = mean(AMT_BALANCE, na.rm = TRUE),
+                                AMT_CREDIT_LIMIT_ACTUAL_MAX = max(AMT_CREDIT_LIMIT_ACTUAL, na.rm = TRUE),
+                                AMT_CREDIT_LIMIT_ACTUAL_MIN = min(AMT_CREDIT_LIMIT_ACTUAL, na.rm = TRUE),
+                                AMT_DRAWINGS_ATM_CURRENT_MEAN = mean(AMT_DRAWINGS_ATM_CURRENT, na.rm = TRUE),
+                                AMT_DRAWINGS_CURRENT_MEAN = mean(AMT_DRAWINGS_CURRENT, na.rm = TRUE),
+                                AMT_DRAWINGS_OTHER_CURRENT_MEAN = mean(AMT_DRAWINGS_OTHER_CURRENT,na.rm = TRUE),
+                                AMT_DRAWINGS_POS_CURRENT_MEAN = mean(AMT_DRAWINGS_POS_CURRENT,na.rm=TRUE),
+                                AMT_INST_MIN_REGULARITY_MEAN = mean(AMT_INST_MIN_REGULARITY,na.rm=TRUE),
+                                AMT_PAYMENT_CURRENT_MEAN = mean(AMT_PAYMENT_CURRENT, na.rm = TRUE),
+                                AMT_PAYMENT_TOTAL_CURRENT_MEAN = mean(AMT_PAYMENT_TOTAL_CURRENT ,na.rm=TRUE),
+                                AMT_RECEIVABLE_PRINCIPAL_MEAN = mean(AMT_RECEIVABLE_PRINCIPAL,na.rm = TRUE),
+                                AMT_RECIVABLE_MEAN = mean(AMT_RECIVABLE, na.rm = TRUE),
+                                AMT_TOTAL_RECEIVABLE_MEAN = mean(AMT_TOTAL_RECEIVABLE,na.rm=TRUE),
+                                CNT_DRAWINGS_ATM_CURRENT_MEAN = mean(CNT_DRAWINGS_ATM_CURRENT, na.rm=TRUE),
+                                CNT_DRAWINGS_CURRENT_MEAN = mean(CNT_DRAWINGS_CURRENT,na.rm=TRUE),
+                                CNT_DRAWINGS_OTHER_CURRENT_MEAN = mean(CNT_DRAWINGS_OTHER_CURRENT,na.rm=TRUE),
+                                CNT_DRAWINGS_POS_CURRENT_MEAN = mean(CNT_DRAWINGS_POS_CURRENT,na.rm=TRUE),
+                                CNT_INSTALMENT_MATURE_CUM_MEAN = mean(CNT_INSTALMENT_MATURE_CUM,na.rm=TRUE),
+                                SK_DPD_SUM = sum(SK_DPD),
+                                SK_DPD_DEF_SUM = sum(SK_DPD_DEF)),by = list(SK_ID_CURR,SK_ID_PREV)]
+  
+  credit_card_balance$NAME_CONTRACT_STATUS[credit_card_balance$NAME_CONTRACT_STATUS != "Active" & credit_card_balance$NAME_CONTRACT_STATUS != "Completed"] = "Other"
+  tempCONTRACT = credit_card_balance[,.N,by = list(SK_ID_CURR, NAME_CONTRACT_STATUS)]
+  tempCONTRACTwide = dcast(tempCONTRACT, SK_ID_CURR ~ NAME_CONTRACT_STATUS, fill = 0)
+  names(tempCONTRACTwide) = c("SK_ID_CURR","NAME_CONTRACT_STATUS_Active", "NAME_CONTRACT_STATUS_Completed","NAME_CONTRACT_STATUS_Other")
+  temp = merge(temp, tempCONTRACTwide, all = TRUE, by = "SK_ID_CURR")
+  
+  temp2 = temp[,.(MONTH_MEAN = mean(N),
+                  AMT_BALANCE_MEAN = mean(AMT_BALANCE_MEAN),
+                  AMT_CREDIT_LIMIT_ACTUAL_MAX = max(AMT_CREDIT_LIMIT_ACTUAL_MAX),
+                  AMT_CREDIT_LIMIT_ACTUAL_MIN = min(AMT_CREDIT_LIMIT_ACTUAL_MIN),
+                  AMT_DRAWINGS_ATM_CURRENT_MEAN = mean(AMT_DRAWINGS_ATM_CURRENT_MEAN),
+                  AMT_DRAWINGS_CURRENT_MEAN = mean(AMT_DRAWINGS_CURRENT_MEAN),
+                  AMT_DRAWINGS_OTHER_CURRENT_MEAN = mean(AMT_DRAWINGS_OTHER_CURRENT_MEAN),
+                  AMT_DRAWINGS_POS_CURRENT_MEAN = mean(AMT_DRAWINGS_POS_CURRENT_MEAN),
+                  AMT_INST_MIN_REGULARITY_MEAN = mean(AMT_INST_MIN_REGULARITY_MEAN),
+                  AMT_PAYMENT_CURRENT_MEAN = mean(AMT_PAYMENT_CURRENT_MEAN),
+                  AMT_PAYMENT_TOTAL_CURRENT_MEAN = mean(AMT_PAYMENT_TOTAL_CURRENT_MEAN),
+                  AMT_RECEIVABLE_PRINCIPAL_MEAN = mean(AMT_RECEIVABLE_PRINCIPAL_MEAN),
+                  AMT_RECIVABLE_MEAN = mean(AMT_RECIVABLE_MEAN),
+                  AMT_TOTAL_RECEIVABLE_MEAN = mean(AMT_TOTAL_RECEIVABLE_MEAN),
+                  CNT_DRAWINGS_ATM_CURRENT_MEAN = mean(CNT_DRAWINGS_ATM_CURRENT_MEAN),
+                  CNT_DRAWINGS_CURRENT_MEAN = mean(CNT_DRAWINGS_CURRENT_MEAN),
+                  CNT_DRAWINGS_OTHER_CURRENT_MEAN = mean(CNT_DRAWINGS_OTHER_CURRENT_MEAN),
+                  CNT_DRAWINGS_POS_CURRENT_MEAN = mean(CNT_DRAWINGS_POS_CURRENT_MEAN),
+                  CNT_INSTALMENT_MATURE_CUM_MEAN = mean(CNT_INSTALMENT_MATURE_CUM_MEAN),
+                  SK_DPD_SUM = sum(SK_DPD_SUM),
+                  SK_DPD_DEF_SUM = sum(SK_DPD_DEF_SUM),
+                  NAME_CONTRACT_STATUS_Active = sum(NAME_CONTRACT_STATUS_Active),
+                  NAME_CONTRACT_STATUS_Completed = sum(NAME_CONTRACT_STATUS_Completed),
+                  NAME_CONTRACT_STATUS_Other = sum(NAME_CONTRACT_STATUS_Other)),by = SK_ID_CURR]
+  
+  if (exploratory == 1){
+    sapply(varCategory, makeplot, "credit_card_balance", "barplot")
+    sapply(varNumeric, makeplot, "credit_card_balance", "density")
+  }
+  
+  credit_card_balance = temp2
   return(credit_card_balance)
 }
 
--- data/readdata_installments_payments.R
+++ data/readdata_installments_payments.R
@@ -1,13 +1,55 @@
-
-readData = function(){
+readData = function(exploratory = 0){
   library(rprojroot)
   library(data.table)
-  requireNamespace("lubridate")
   root = find_root(is_git_root)
   setwd(root)
+  ########################################################################################
+  # Problem: 1. NUM_INSTALMENT_VERSION的处理
+  # 2. NAs in DAYS_ENTRY_PAYMENT, AMT_PAYMENT
+  ########################################################################################
+  makeplot <- function(var, data, type){
+    pdf(file.path(data,"_",var,".pdf",fsep = ""), 
+        width = 14, 
+        height = 12)
+    par(cex.lab=1.5,cex.axis=1.5,mar=c(8,5,2,2) + 0.1,lwd=2)
+    if (type == "barplot"){
+      eval(parse(text = file.path("barplot(",data,"[,.N,by =", 
+                                  var,
+                                  "]$N,names.arg  = ",data,"[,.N,by =",
+                                  var,
+                                  "]$",
+                                  var,
+                                  ",las = 2)",
+                                  fsep = "")))
+    } else if (type == "density") {
+      eval(parse(text = file.path("plot(density(na.omit(",data,"$", 
+                                  var,
+                                  ")))",
+                                  fsep = "")))
+    }
+    dev.off()
+  }
   
   installments_payments = fread("../data/installments_payments.csv", na.strings = "") 
+  sort(sapply(installments_payments,function(x) sum(is.na(x))))
   
+  installments_payments[, INSTALMENTS_DPD := DAYS_ENTRY_PAYMENT - DAYS_INSTALMENT] # 逾期天数 >0 代表逾期
+  installments_payments[, INSTALMENTS_LESS := AMT_PAYMENT - AMT_INSTALMENT]        # 少还金额 <0 代表少还了
+  
+  temp  = installments_payments[,.(NUM_INSTALMENT_VERSION_MEAN = mean(NUM_INSTALMENT_VERSION),
+                                   NUM_INSTALMENT_NUMBER_MEAN = mean(NUM_INSTALMENT_NUMBER),
+                                   DAYS_INSTALMENT_MEAN = mean(DAYS_INSTALMENT),
+                                   DAYS_ENTRY_PAYMENT_MEAN = mean(DAYS_ENTRY_PAYMENT, na.rm = TRUE),
+                                   AMT_INSTALMENT_MEAN = mean(AMT_INSTALMENT),
+                                   AMT_PAYMENT_MEAN = mean(AMT_PAYMENT, na.rm = TRUE),
+                                   INSTALMENTS_DPD_MEAN = mean(INSTALMENTS_DPD, na.rm = TRUE),
+                                   INSTALMENTS_LESS_MEAN = mean(INSTALMENTS_LESS, na.rm=TRUE)), by = SK_ID_CURR]
+  
+  if (exploratory == 1){
+    sapply(names(installments_payments), makeplot, "installments_payments", "density")
+  }
+  
+  installments_payments = temp
   return(installments_payments)
 }
 
