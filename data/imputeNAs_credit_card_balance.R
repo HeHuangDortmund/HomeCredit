2bfabab6869f80a9976515eb928a9c3617a53221
Fang Duan <fang.duan.hu@gmail.com>
2018-07-02 15:46:03 +0200

    微调ISNA variable

--- data/imputeNAs_credit_card_balance.R
+++ data/imputeNAs_credit_card_balance.R
@@ -1,6 +1,6 @@
 imputeNA <- function(application){
-  application$NA_AMT_DRAWINGS_CURRENT_MEAN = as.integer(is.na(application$AMT_DRAWINGS_CURRENT_MEAN))
-  application$NA_AMT_DRAWINGS_ATM_CURRENT_MEAN = as.integer(is.na(application$AMT_DRAWINGS_ATM_CURRENT_MEAN))
+  application$NA_AMT_MEAN = as.integer(is.na(application$AMT_BALANCE_MEAN))
+  application$NA_AMT_DRAWINGS_MEAN = as.integer(is.na(application$AMT_DRAWINGS_ATM_CURRENT_MEAN))
   application = impute(as.data.frame(application), 
                        target = "TARGET", 
                        classes = list(numeric = imputeMean(),
--- data/imputeNAs_previous_application.R
+++ data/imputeNAs_previous_application.R
@@ -1,8 +1,9 @@
 imputeNA <- function(application){
-  application$NA_DAYS_FIRST_LAST_MEAN = as.integer(is.na(application$DAYS_FIRST_DRAWING_MEAN))
-  application$NA_DOWN_PAYMENT_MEAN = as.integer(is.na(application$AMT_DOWN_PAYMENT_MEAN))
+  application$NA_PREV_APPLICATION  = as.integer(is.na(application$CNT_PREV))
+  application$NA_AMT_DOWN_PAYMENT_MEAN = as.integer(is.na(application$AMT_DOWN_PAYMENT_MEAN))
+  application$NA_DAYS_TERMINATION_MEAN = as.integer(is.na(application$DAYS_TERMINATION_MEAN))
+  application$NA_DAYS_FIRST_DRAWING_MEAN = as.integer(is.na(application$DAYS_FIRST_DRAWING_MEAN))
   application$NA_RATE_INTEREST_MEAN = as.integer(is.na(application$RATE_INTEREST_PRIMARY_MEAN))
-  
   application = impute(as.data.frame(application), 
                        target = "TARGET", 
                        classes = list(numeric = imputeMean(),
