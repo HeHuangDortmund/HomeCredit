859cb4b1ec3b54981f0509161e68628432d7133e
Fang Duan <fang.duan.hu@gmail.com>
2018-07-05 21:14:52 +0200

    增添选项: drop NA过多的变量

--- data/imputeNAs_credit_card_balance.R
+++ data/imputeNAs_credit_card_balance.R
@@ -1,10 +1,19 @@
-imputeNA <- function(application){
-  application$NA_AMT_MEAN = as.integer(is.na(application$AMT_BALANCE_MEAN))
-  application$NA_AMT_DRAWINGS_MEAN = as.integer(is.na(application$AMT_DRAWINGS_ATM_CURRENT_MEAN))
-  application = impute(as.data.frame(application), 
-                       target = "TARGET", 
-                       classes = list(numeric = imputeMean(),
-                                      integer = imputeMean()))$data
+imputeNA <- function(application,
+                     naDrop = FALSE){
+  if (naDrop == FALSE){
+    application$NA_AMT_MEAN = as.integer(is.na(application$AMT_BALANCE_MEAN))
+    application$NA_AMT_DRAWINGS_MEAN = as.integer(is.na(application$AMT_DRAWINGS_ATM_CURRENT_MEAN))
+    application = impute(as.data.frame(application), 
+                         target = "TARGET", 
+                         classes = list(numeric = imputeMean(),
+                                        integer = imputeMean()))$data
+  } else {
+    missValue = sapply(application, function(x) sum(is.na(x))) 
+    missValue = missValue[missValue > 0]/dim(application)[1]
+    varDrop = names(missValue)[missValue > 0.5]
+    setDT(application)
+    application = application[,c(varDrop) := NULL]
+  }  
   application = as.data.table(application)
   return(application)
 }
--- data/imputeNAs_previous_application.R
+++ data/imputeNAs_previous_application.R
@@ -1,13 +1,32 @@
-imputeNA <- function(application){
-  application$NA_PREV_APPLICATION  = as.integer(is.na(application$CNT_PREV))
-  application$NA_AMT_DOWN_PAYMENT_MEAN = as.integer(is.na(application$AMT_DOWN_PAYMENT_MEAN))
-  application$NA_DAYS_TERMINATION_MEAN = as.integer(is.na(application$DAYS_TERMINATION_MEAN))
-  application$NA_DAYS_FIRST_DRAWING_MEAN = as.integer(is.na(application$DAYS_FIRST_DRAWING_MEAN))
-  application$NA_RATE_INTEREST_MEAN = as.integer(is.na(application$RATE_INTEREST_PRIMARY_MEAN))
-  application = impute(as.data.frame(application), 
-                       target = "TARGET", 
-                       classes = list(numeric = imputeMean(),
-                                      integer = imputeMean()))$data
+imputeNA <- function(application,
+                     naDrop = FALSE){
+  if (naDrop == FALSE){
+    application$NA_PREV_APPLICATION  = as.integer(is.na(application$CNT_PREV))
+    application$NA_AMT_DOWN_PAYMENT_MEAN = as.integer(is.na(application$AMT_DOWN_PAYMENT_MEAN))
+    application$NA_DAYS_TERMINATION_MEAN = as.integer(is.na(application$DAYS_TERMINATION_MEAN))
+    application$NA_DAYS_FIRST_DRAWING_MEAN = as.integer(is.na(application$DAYS_FIRST_DRAWING_MEAN))
+    application$NA_RATE_INTEREST_MEAN = as.integer(is.na(application$RATE_INTEREST_PRIMARY_MEAN))
+    application = impute(as.data.frame(application), 
+                         target = "TARGET", 
+                         classes = list(numeric = imputeMean(),
+                                        integer = imputeMean()))$data
+  } else {
+    missValue = sapply(application, function(x) sum(is.na(x))) 
+    missValue = missValue[missValue > 0]/dim(application)[1]
+    # barplot(missValue,las = 2,cex.lab=0.2)
+    varDrop = names(missValue)[missValue > 0.5]
+    # Note: EXT_SOURCE_1 is also dropped
+    setDT(application)
+    application = application[,c(varDrop) := NULL]
+    
+    application$NA_PREV_APPLICATION  = as.integer(is.na(application$CNT_PREV))
+    application$NA_AMT_DOWN_PAYMENT_MEAN = as.integer(is.na(application$AMT_DOWN_PAYMENT_MEAN))
+    application$NA_DAYS_TERMINATION_MEAN = as.integer(is.na(application$DAYS_TERMINATION_MEAN))
+    application = impute(as.data.frame(application), 
+                         target = "TARGET", 
+                         classes = list(numeric = imputeMean(),
+                                        integer = imputeMean()))$data
+  }
   application = as.data.table(application)
   return(application)
 }
