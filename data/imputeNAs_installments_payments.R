68d306b375eb72a2622949ddf537ffebdf867e76
Fang Duan <fang.duan.hu@gmail.com>
2018-07-13 17:52:30 +0200

    update几个变量名称

--- data/imputeNAs_installments_payments.R
+++ data/imputeNAs_installments_payments.R
@@ -1,5 +1,5 @@
 imputeNA <- function(application){
-  application$NA_INSTALLMENT_PAYMENT = as.integer(is.na(application$NUM_INSTALMENT_VERSION_MEAN))
+  application$NA_INSTALLMENT_PAYMENT = as.integer(is.na(application$AMT_INSTALMENT_MEAN))
   application = impute(as.data.frame(application), 
                        target = "TARGET", 
                        classes = list(numeric = imputeMean()))$data
