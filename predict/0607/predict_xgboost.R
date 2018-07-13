d0d279c8f3b8579a4dc1d6ccb041f83b1b059d85
Fang Duan <fang.duan.hu@gmail.com>
2018-07-13 17:58:25 +0200

    7月6日的一次尝试(xgboost), 忘记commit

--- predict/0607/predict_xgboost.R
+++ predict/0607/predict_xgboost.R
@@ -1,4 +1,4 @@
-pkgs <- c("rprojroot","data.table","checkmate","mlr","psych", "xgboost","corrplot")
+pkgs <- c("rprojroot","data.table","checkmate","mlr","psych", "xgboost","corrplot","randomForest")
 lapply(pkgs,require,character.only = TRUE)
 requireNamespace("withr")
 root = find_root(is_git_root)
@@ -14,6 +14,21 @@ nameFactor = names(data)[unlist(lapply(data, function(x) is.factor(x)))]
 
 #data = normalizeFeatures(as.data.frame(data), target = "TARGET", cols = nameNumeric, method = "center")
 #data = normalizeFeatures(as.data.frame(data), target = "TARGET", cols = nameNumeric, method = "scale")
+
+varNADrop = c("NA_YEARS_BEGINEXPLUATATION",
+              "NA_FLOORSMAX",
+              "NA_TOTALAREA_MODE",
+              "NA_BUREAU",
+              "NA_DAYS_CREDIT_ENDDATE",
+              "NA_INSTALLMENT_PAYMENT")
+varREDrop = c("YEARS_BEGINEXPLUATATION_MODE",
+              "FLOORSMAX_MODE",
+              "LIVINGAREA_MODE",
+              "YEARS_BEGINEXPLUATATION_MEDI",
+              "FLOORSMAX_MEDI",
+              "LIVINGAREA_MEDI")
+
+data = data[,c(varNADrop,varREDrop) := NULL]
 
 data = createDummyFeatures(as.data.frame(data), target = "TARGET", 
                            cols = nameFactor,
@@ -35,27 +50,30 @@ train[constan]= NULL
 test[constan] = NULL
 # temp = removeConstantFeatures(train)
 train.task = makeClassifTask(id = "class", data = as.data.frame(train), target = "TARGET")
+
+fv = generateFilterValuesData(train.task, method = "randomForest.importance")
+
 xgb_learner = makeLearner("classif.xgboost", 
                           predict.type = "prob",
                           par.vals = list(
                             objective = "binary:logistic",
                             eval_metric = "auc",
-                            nrounds = 50,
+                            nrounds = 100,
                             verbose = 1
                           ))
 
-xgb_params = makeParamSet(makeIntegerParam("max_depth", lower = 5, upper = 10),
-                          makeNumericParam("eta",lower = 0.1, upper = 0.5))
-resample_desc = makeResampleDesc("CV", iters = 5)
-control = makeTuneControlRandom(maxit = 10)
-tuned_params = tuneParams(learner = xgb_learner,
-                          task = train.task,
-                          resampling = resample_desc,
-                          par.set = xgb_params,
-                          control = control)
-
-xgb_tuned_learner = setHyperPars(learner = xgb_learner, par.vals = tuned_params$x)
-xgb_model = train(xgb_tuned_learner, train.task)
+# xgb_params = makeParamSet(makeIntegerParam("max_depth", lower = 5, upper = 10),
+#                           makeNumericParam("eta",lower = 0.1, upper = 0.5))
+# resample_desc = makeResampleDesc("CV", iters = 5)
+# control = makeTuneControlRandom(maxit = 10)
+# tuned_params = tuneParams(learner = xgb_learner,
+#                           task = train.task,
+#                           resampling = resample_desc,
+#                           par.set = xgb_params,
+#                           control = control)
+# 
+# xgb_tuned_learner = setHyperPars(learner = xgb_learner, par.vals = tuned_params$x)
+# xgb_model = train(xgb_tuned_learner, train.task)
 
 xgb_model = train(xgb_learner, task = train.task)
 result = predict(xgb_model, newdata = test)
