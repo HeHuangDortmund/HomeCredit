f19eea32902471b3a8899885153c7ba17aa4f8ed
Fang Duan <fang.duan.hu@gmail.com>
2018-07-16 23:48:56 +0200

    drop掉几个变量, 求feature importance

--- workflow_glance.R
+++ workflow_glance.R
@@ -1,51 +1,51 @@
-library("rprojroot")
-library("data.table")
-library("checkmate")
-library("ggplot2")
-library("corrplot")
+rm(list=ls())
+pkgs <- c("rprojroot","data.table","checkmate","mlr","ggplot2","corrplot","randomForest")
+lapply(pkgs,require,character.only = TRUE)
 requireNamespace("withr")
 root = find_root(is_git_root)
 set.seed(1)
 
 # TODO
-#0. 检查ISNA_indicator是否高度相关(see below)
+#0. 检查ISNA_indicator是否高度相关(done, see below)
 #1. 尝试找出category变量中特定level与TARGET的关联, 避免在合并categories时损失过多信息 (解决办法:作图来看不明显, 对于类型太多的变量使用hash trick)
-#2. 手动删除NA太多的变量(done, see readData2.R)
-#3. 手动删除意义上重复变量, 如MOD,AVG,MEDI (未做)
+#2. 手动删除NA太多的变量(partially done, see readData2.R)
+#3. 手动删除意义上重复变量, 如MOD,AVG,MEDI (partially done)
 #4. 特征选择: randomforest和mlr (未做)
 #5. 建模: logistic, NN(与ROC等价的loss function), xgboost (未做)
 
 # read data
 source(file.path(root,"data", "readData2.R"))
 data = readData(version = 3) # version = 3 还未完成
-train = data[data$split == "train",]
-test = data[data$split == "test",]
 
 ##ISNA_indicator是否高度相关###########################################################
 matrix.cor = cor(data[,.SD,.SDcols = names(data)[grep("NA_",names(data))]])
-pdf("corr_ISNA_variable.pdf",width = 14, height = 14)
+pdf(file.path(root,"plots", "corr_ISNA_variable.pdf"),width = 14, height = 14)
 corrplot.mixed(matrix.cor, tl.col = "black", tl.pos = "lt")
 dev.off()
 ## 以下几类变量相互corr较高 (>=0.9), 手动删除NA太多变量之后
-# 1. NA_YEARS_BEGINEXPLUATATION, NA_FLOORSMAX, NA_LIVINGAREA, NA_TOTALAREA_MODE
+# 1. NA_TOTALAREA_MODE, NA_YEARS_BEGINEXPLUATATION, NA_FLOORSMAX, NA_LIVINGAREA, NA_ENTRANCES, NA_APARTMENT, NA_ELEVATORS
 # 2. NA_AMT_REQ, NA_BUREAU, NA_DAYS_CREDIT_ENDDATE
 # 3. NA_POS_CASH, NA_INSTALLMENT_PAYMENT, NA_PREV_APPLICATION
 
-varNADrop = c("NA_YEARS_BEGINEXPLUATATION",
+# drop掉相关性太高的ISNA变量
+varDrop_ISNA = c("NA_YEARS_BEGINEXPLUATATION",
              "NA_FLOORSMAX",
-             "NA_TOTALAREA_MODE",
+             "NA_LIVINGAREA",
+             "NA_ENTRANCES",
+             "NA_APARTMENT", 
+             "NA_ELEVATORS",
              "NA_BUREAU",
              "NA_DAYS_CREDIT_ENDDATE",
              "NA_INSTALLMENT_PAYMENT")
-varREDrop = c("YEARS_BEGINEXPLUATATION_MODE",
-              "FLOORSMAX_MODE",
-              "LIVINGAREA_MODE",
-              "YEARS_BEGINEXPLUATATION_MEDI",
-              "FLOORSMAX_MEDI",
-              "LIVINGAREA_MEDI")
+# 去除意义上重复的变量
+varDrop_Replicated = c("YEARS_BEGINEXPLUATATION_MODE",
+                       "FLOORSMAX_MODE",
+                       "LIVINGAREA_MODE",
+                       "YEARS_BEGINEXPLUATATION_MEDI",
+                       "FLOORSMAX_MEDI",
+                       "LIVINGAREA_MEDI")
 
-data = data[,c(varNADrop,varREDrop) := NULL]
-
+data = data[,c(varDrop_ISNA,varDrop_Replicated) := NULL]
 ## 以下几类变量相互corr较高 (>=0.9), 未手动删除NA太多变量
 # 1. NA_YEARS_BEGINEXPLUATATION, NA_FLOORSMAX, NA_ENTRANCES, NA_APARTMENT, NA_ELEVATORS, NA_NONLIVINGAREA, NA_TOTALAREA_MODE
 # 2. NA_LANDAREA, NA_LIVINGAREA
@@ -53,63 +53,37 @@ data = data[,c(varNADrop,varREDrop) := NULL]
 # 4. NA_DAYS_CREDIT_ENDDATE, NA_BUREAU, NA_AMT_REQ
 # 5. NA_INSTALLMENT_PAYMENT, NA_POS_CASH, NA_PREV_APPLICATION, NA_DAYS_FIRST_DRAWING_MEAN, NA_AMT_DRAWINGS_MEAN
 
-##plot categorical variables###############################################################
-source(file.path(root,"data", "readData2.R"))
-application = readData(version = 1)
+nameFactor = names(data)[unlist(lapply(data, function(x) is.factor(x)))]
+data = createDummyFeatures(as.data.frame(data), target = "TARGET", 
+                           cols = nameFactor,
+                           method = "reference")
+train = data[data$split == "train",]
+test = data[data$split == "test",]
 
-# application
-varCategory = names(application)[sapply(application, function(x) is.factor(x))]
-train = application[split == "train"]
+train$split = NULL
+test$split = NULL
 
-makeplot_category <- function(var){
-  eval(parse(text = file.path("ggplot(data=train, aes(x=",
-                              var,
-                              "))+geom_bar(stat=\"count\", position = \"fill\", aes(fill = as.factor(TARGET))) + labs(x = \"\", fill = \"TARGET\") + coord_flip()",fsep = "")))
-  ggsave(file.path("stacked_percentage_barplot_application_",var,".pdf",fsep= ""))
-}
-sapply(varCategory,makeplot_category)
+SK_ID_CURR = test$SK_ID_CURR
+train$SK_ID_CURR = NULL
+test$SK_ID_CURR = NULL
+test$TARGET = NULL
 
-# previous_application
-previous_application = fread("../data/previous_application.csv", na.strings = "")
-varCategory = names(previous_application)[sapply(previous_application, function(x) is.character(x))]
-previous_application = previous_application[,.SD,.SDcols = varCategory,by = list(SK_ID_CURR,SK_ID_PREV)]
-# Replace XNA,XAP
-previous_application[previous_application == "XNA"] = "miss"
-previous_application[previous_application == "XAP"] = "miss"
-previous_application[is.na(previous_application)] = "miss"
+const = unlist(lapply(train, function(x) sd(x) == 0))
+const = names(train)[const]
+train[const]= NULL
+test[const] = NULL
 
-application_subset = application[,.SD,.SDcols = c("SK_ID_CURR","TARGET","split")]
-merged = merge(previous_application, application_subset, all.x = TRUE, by = "SK_ID_CURR")
-merged = merged[split == "train"]
+#### feature importance
+# train.task = makeClassifTask(id = "class", data = as.data.frame(train), target = "TARGET")
+# fv = generateFilterValuesData(train.task, method = "randomForest.importance")
+start_time = Sys.time()
+quick_RF = randomForest(train[,-88],train[,88], ntree = 30, importance = TRUE)
+end_time = Sys.time()
+imp_RF <- importance(quick_RF)
+imp_DF <- data.frame(Variables = row.names(imp_RF), MSE = imp_RF[,1])
+imp_DF <- imp_DF[order(imp_DF$MSE, decreasing = TRUE),]
 
-makeplot_category <- function(var){
-  eval(parse(text = file.path("ggplot(data=merged, aes(x=",
-                              var,
-                              "))+geom_bar(stat=\"count\", position = \"fill\", aes(fill = as.factor(TARGET))) + labs(x = \"\", fill = \"TARGET\") + coord_flip()",fsep = "")))
-  ggsave(file.path("stacked_percentage_barplot_previous_application_",var,".pdf",fsep= ""))
-}
-sapply(varCategory,makeplot_category)
-
-# credit_card_balance
-credit_card_balance = fread("../data/credit_card_balance.csv", na.strings = "")
-merged = merge(credit_card_balance, application_subset, all.x = TRUE, by = "SK_ID_CURR")
-merged = merged[split == "train"]
-ggplot(data=merged, aes(x=NAME_CONTRACT_STATUS))+
-  geom_bar(stat="count", position = "fill", aes(fill = as.factor(TARGET))) + 
-  labs(x = "", fill = "TARGET") + 
-  coord_flip()
-ggsave("stacked_percentage_barplot_credit_card_balance_NAME_CONTRACT_STATUS.pdf")
-
-# POS_CASH_balance
-POS_CASH_balance = fread("../data/POS_CASH_balance.csv", na.strings = "")
-merged = merge(POS_CASH_balance, application_subset, all.x = TRUE, by = "SK_ID_CURR")
-merged = merged[split == "train"]
-ggplot(data=merged, aes(x=NAME_CONTRACT_STATUS))+
-  geom_bar(stat="count", position = "fill", aes(fill = as.factor(TARGET))) + 
-  labs(x = "", fill = "TARGET") + 
-  coord_flip()
-ggsave("stacked_percentage_barplot_POS_CASH_balance_NAME_CONTRACT_STATUS.pdf")
-
+ggplot(imp_DF[1:20,], aes(x=reorder(Variables, MSE), y=MSE, fill=MSE)) + geom_bar(stat = 'identity') + labs(x = 'Variables', y= '% increase MSE if variable is randomly permuted') + coord_flip() + theme(legend.position="none")
 # add feature SUM_OVERDUE
 source(file.path(root, "preproc", "add_feature_SUM_OVERDUE.R"))
 data = preproc(data)
