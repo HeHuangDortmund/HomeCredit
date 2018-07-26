41e68e359acdf19b06734b2ac78859fc58a2c5f0
Win7Adm <fang.duan.hu@gmail.com>
2018-07-26 20:51:14 +0200

    1. 在第一步对6个变量就用0填NA 2. add new features before aggregation

--- data/imputeNAs_credit_card_balance.R
+++ data/imputeNAs_credit_card_balance.R
@@ -1,8 +1,7 @@
 imputeNA <- function(application,
                      naDrop = FALSE){
   if (naDrop == FALSE){
-    application$NA_AMT_MEAN = as.integer(is.na(application$AMT_BALANCE_MEAN))
-    application$NA_AMT_DRAWINGS_MEAN = as.integer(is.na(application$AMT_DRAWINGS_ATM_CURRENT_MEAN))
+    application$NA_CREDIT_CARD_BALANCE = as.integer(is.na(application$AMT_BALANCE_MEAN))
     application = impute(as.data.frame(application), 
                          target = "TARGET", 
                          classes = list(numeric = imputeMean(),
--- data/readdata_credit_card_balance.R
+++ data/readdata_credit_card_balance.R
@@ -8,21 +8,46 @@ readData = function(version = 1){
   ########################################################################################
   credit_card_balance = fread("../data/credit_card_balance.csv", na.strings = "")
 
-  if (version == 2){ # aggregation之前先用0填补一部分NA, version==1 直接汇总
-    credit_card_balance$AMT_DRAWINGS_ATM_CURRENT[is.na(credit_card_balance$AMT_DRAWINGS_ATM_CURRENT)] = 0
-    credit_card_balance$AMT_DRAWINGS_OTHER_CURRENT[is.na(credit_card_balance$AMT_DRAWINGS_OTHER_CURRENT)] = 0
-    credit_card_balance$AMT_DRAWINGS_POS_CURRENT[is.na(credit_card_balance$AMT_DRAWINGS_POS_CURRENT)] = 0
-    credit_card_balance$CNT_DRAWINGS_ATM_CURRENT[is.na(credit_card_balance$CNT_DRAWINGS_ATM_CURRENT)] = 0
-    credit_card_balance$CNT_DRAWINGS_OTHER_CURRENT[is.na(credit_card_balance$CNT_DRAWINGS_OTHER_CURRENT)] = 0
-    credit_card_balance$CNT_DRAWINGS_POS_CURRENT[is.na(credit_card_balance$CNT_DRAWINGS_POS_CURRENT)] = 0
-    credit_card_balance$AMT_INST_MIN_REGULARITY[is.na(credit_card_balance$AMT_INST_MIN_REGULARITY)] = 0
-    credit_card_balance$AMT_PAYMENT_CURRENT[is.na(credit_card_balance$AMT_PAYMENT_CURRENT)] = 0
-    credit_card_balance$CNT_INSTALMENT_MATURE_CUM[is.na(credit_card_balance$CNT_INSTALMENT_MATURE_CUM)] = 0 # 暂时补0,需merge installments 信息
-  }
-
+  # aggregation之前先用0填NA, 以下6个变量为NA时AMT_DRAWINGS_ATM_CURRENT为0， AMT_DRAWINGS_CURRENT = AMT_DRAWINGS_ATM_CURRENT + AMT_DRAWINGS_OTHER_CURRENT + AMT_DRAWINGS_POS_CURRENT
+  credit_card_balance$AMT_DRAWINGS_ATM_CURRENT[is.na(credit_card_balance$AMT_DRAWINGS_ATM_CURRENT)] = 0
+  credit_card_balance$AMT_DRAWINGS_OTHER_CURRENT[is.na(credit_card_balance$AMT_DRAWINGS_OTHER_CURRENT)] = 0
+  credit_card_balance$AMT_DRAWINGS_POS_CURRENT[is.na(credit_card_balance$AMT_DRAWINGS_POS_CURRENT)] = 0
+  credit_card_balance$CNT_DRAWINGS_ATM_CURRENT[is.na(credit_card_balance$CNT_DRAWINGS_ATM_CURRENT)] = 0
+  credit_card_balance$CNT_DRAWINGS_OTHER_CURRENT[is.na(credit_card_balance$CNT_DRAWINGS_OTHER_CURRENT)] = 0
+  credit_card_balance$CNT_DRAWINGS_POS_CURRENT[is.na(credit_card_balance$CNT_DRAWINGS_POS_CURRENT)] = 0
+  # AMT_PAYMENT_CURRENT = NA 时 所有AMT_PAYMENT_TOTAL_CURRENT = 0， 因此填0
+  credit_card_balance$AMT_PAYMENT_CURRENT[is.na(credit_card_balance$AMT_PAYMENT_CURRENT) & 
+                                          credit_card_balance$AMT_PAYMENT_TOTAL_CURRENT == 0] = 0
+  
+  # AMT_INST_MIN_REGULARITY和CNT_INSTALMENT_MATURE_CUM两个变量缺失时，大部分未发生transaction（即AMT_RECIVABLE,AMT_PAYMENT_CURRENT,AMT_DRAWINGS_CURRENT均为0），这部分用0填补，填补后剩余33822 NA
+  credit_card_balance$AMT_INST_MIN_REGULARITY[is.na(credit_card_balance$AMT_INST_MIN_REGULARITY) & 
+                                              credit_card_balance$AMT_RECIVABLE == 0 & 
+                                              credit_card_balance$AMT_PAYMENT_CURRENT == 0 &
+                                              credit_card_balance$AMT_DRAWINGS_CURRENT == 0] = 0
+  credit_card_balance$CNT_INSTALMENT_MATURE_CUM[is.na(credit_card_balance$CNT_INSTALMENT_MATURE_CUM) & 
+                                                credit_card_balance$AMT_RECIVABLE == 0 & 
+                                                credit_card_balance$AMT_PAYMENT_CURRENT == 0 &
+                                                credit_card_balance$AMT_DRAWINGS_CURRENT == 0] = 0
+  
+  # add hand maded features
+  credit_card_balance[,`:=`(Add_DIFF_BALANCE_LIMIT_CARD = AMT_BALANCE - AMT_CREDIT_LIMIT_ACTUAL,
+                            Add_DIFF_DRAWINGS_LIMIT_CARD = AMT_DRAWINGS_CURRENT - AMT_CREDIT_LIMIT_ACTUAL,
+                            Add_DIFF_PAYMENT_INST_MIN_CARD =  AMT_PAYMENT_CURRENT - AMT_INST_MIN_REGULARITY,
+                            Add_DIFF_PAYMENT_DRAWINGS_CARD = AMT_PAYMENT_CURRENT - AMT_DRAWINGS_CURRENT,
+                            Add_DIFF_PAYMENT_RECEIVABLE_CARD = AMT_PAYMENT_CURRENT - AMT_RECIVABLE,
+                            Add_DIFF_PAYMENT_RECEIVABLE_PRINCIPAL_CARD = AMT_PAYMENT_CURRENT - AMT_RECEIVABLE_PRINCIPAL,
+                            Add_DIFF_RECEIVABLE_BALANCE_CARD = AMT_RECIVABLE - AMT_BALANCE,
+                            Add_DIFF_RECEIVABLE_PRINCIPAL_BALANCE_CARD = AMT_RECEIVABLE_PRINCIPAL - AMT_BALANCE,
+                            # Add_CARD_DRAWINGS_CURRENT_per_CNT = AMT_DRAWINGS_CURRENT / CNT_DRAWINGS_CURRENT, # Problematic cases: all AMT&CNT (CURRENT,ATM,POS,OTHER) =0 but AMT_CURRENT !=0
+                            Add_CARD_DRAWINGS_ATM_per_CNT = AMT_DRAWINGS_ATM_CURRENT / CNT_DRAWINGS_ATM_CURRENT,
+                            Add_CARD_DRAWINGS_OTHER_per_CNT = AMT_DRAWINGS_OTHER_CURRENT / CNT_DRAWINGS_OTHER_CURRENT,
+                            Add_CARD_DRAWINGS_POS_per_CNT = AMT_DRAWINGS_POS_CURRENT / CNT_DRAWINGS_POS_CURRENT
+  ), by = list(SK_ID_CURR,SK_ID_PREV)]
+  credit_card_balance[credit_card_balance == "NaN"] = 0 # 填补后3个feature中CNT或者AMT是0时造成的NaN，i.e. 0/0
+  
   # 对每个变量通过求MAX,SUM和MEAN进行汇总, 以SK_ID_PREV
-  varMEAN = setdiff(names(credit_card_balance),c("SK_ID_CURR","SK_ID_PREV","NAME_CONTRACT_STATUS"))
-  varSUMMAX = c("SK_DPD", "SK_DPD_DEF","AMT_CREDIT_LIMIT_ACTUAL","CNT_INSTALMENT_MATURE_CUM")
+  varMEAN = setdiff(names(credit_card_balance),c("SK_ID_CURR","SK_ID_PREV","NAME_CONTRACT_STATUS","MONTHS_BALANCE"))
+  varSUMMAX = c("SK_DPD", "SK_DPD_DEF","AMT_CREDIT_LIMIT_ACTUAL","CNT_INSTALMENT_MATURE_CUM",names(credit_card_balance)[grep("Add",names(credit_card_balance))])
   temp_mean = credit_card_balance[,lapply(.SD, mean, na.rm = TRUE), .SDcols = varMEAN, by = list(SK_ID_CURR,SK_ID_PREV)]
   names(temp_mean)[-c(1,2)] = paste(names(temp_mean)[-c(1,2)],"MEAN",sep = "_")
   temp_summax = credit_card_balance[,c(lapply(.SD, sum, na.rm = TRUE),
@@ -47,8 +72,9 @@ readData = function(version = 1){
   temp_cat_wide = temp_cat_wide[, lapply(.SD, sum, na.rm = TRUE), .SDcols = names(temp_cat_wide)[-c(1,2)], by = SK_ID_CURR]
   names(temp_cat_wide)[-1] = paste(names(temp_cat_wide)[-1], "SUM", sep = "_")
   temp_mean = temp_mean[,lapply(.SD, mean, na.rm = TRUE), .SDcols = names(temp_mean)[-c(1,2)], by = SK_ID_CURR]
-  temp_sum = temp_summax[, lapply(.SD, sum, na.rm = TRUE), .SDcols = c("SK_DPD_SUM","SK_DPD_DEF_SUM","AMT_CREDIT_LIMIT_ACTUAL_SUM"), by = SK_ID_CURR]
-  temp_max = temp_summax[, lapply(.SD, max, na.rm = TRUE), .SDcols = c("SK_DPD_MAX","SK_DPD_DEF_MAX","AMT_CREDIT_LIMIT_ACTUAL_MAX"), by = SK_ID_CURR]
+  temp_sum = temp_summax[, lapply(.SD, sum, na.rm = TRUE), .SDcols = c("SK_DPD_SUM","SK_DPD_DEF_SUM","AMT_CREDIT_LIMIT_ACTUAL_SUM","CNT_INSTALMENT_MATURE_CUM_SUM"), by = SK_ID_CURR]
+  temp_max = temp_summax[, lapply(.SD, max, na.rm = TRUE), .SDcols = c("SK_DPD_MAX","SK_DPD_DEF_MAX","AMT_CREDIT_LIMIT_ACTUAL_MAX","CNT_INSTALMENT_MATURE_CUM_MAX",
+                                                                       paste(names(credit_card_balance)[grep("Add",names(credit_card_balance))], "MAX", sep="_")), by = SK_ID_CURR]
   
   # merge
   temp_all = merge(temp_month, temp_mean,all = TRUE, by = c("SK_ID_CURR"))
