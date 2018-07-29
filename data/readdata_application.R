c44e9e2ac276c5ba34ef2e34f00a941602658b91
Fang Duan <fang.duan.hu@gmail.com>
2018-07-29 22:56:20 +0200

    调整一些hand made features, 将application新加的feature移到readdata中

--- data/readdata_application.R
+++ data/readdata_application.R
@@ -19,6 +19,7 @@ readData = function(){
   
   # 365243 in `DAYS` variable means NA
   application$DAYS_EMPLOYED[application$DAYS_EMPLOYED == 365243] = NA 
+  application$DAYS_EMPLOYED = as.numeric(application$DAYS_EMPLOYED)
   
   ## 2. Category Variablen as Factor or Interger
   numberOfValues = unlist(lapply(application, function(x){length(unique(x))}))
@@ -47,5 +48,21 @@ readData = function(){
   application[, (columsBinar) := lapply(.SD, function(x) as.integer(x)-1),
               .SDcols = columsBinar] 
   
+  # add new features
+  application[,`:=`(Add_RATIO_ANNUITY_INCOME = AMT_ANNUITY / AMT_INCOME_TOTAL,
+                    Add_RATIO_CREDIT_INCOME = AMT_CREDIT / AMT_INCOME_TOTAL,
+                    Add_RATIO_CREDIT_GOODS = AMT_CREDIT / AMT_GOODS_PRICE, 
+                    Add_RATIO_PAYMENT = AMT_ANNUITY / AMT_CREDIT, 
+                    Add_RATIO_EMPLOYED_BIRTH = DAYS_EMPLOYED / DAYS_BIRTH,
+                    Add_RATIO_CAR_BIRTH = OWN_CAR_AGE / DAYS_BIRTH,
+                    Add_RATIO_CAR_EMPLOYED = OWN_CAR_AGE / DAYS_EMPLOYED, 
+                    Add_RATIO_PHONE_BIRTH = DAYS_LAST_PHONE_CHANGE / DAYS_BIRTH,
+                    Add_RATIO_PHONE_EMPLOYED = DAYS_LAST_PHONE_CHANGE / DAYS_EMPLOYED,
+                    Add_RATIO_CHILD = CNT_CHILDREN / CNT_FAM_MEMBERS,
+                    Add_INCOME_per_FAM = AMT_INCOME_TOTAL / CNT_FAM_MEMBERS,
+                    Add_INCOME_per_ADULT = AMT_INCOME_TOTAL / (CNT_FAM_MEMBERS - CNT_CHILDREN),
+                    Add_CREDIT_per_FAM = AMT_CREDIT / CNT_FAM_MEMBERS,
+                    Add_CREDIT_per_ADULT = AMT_CREDIT / (CNT_FAM_MEMBERS - CNT_CHILDREN))]
+  application$Add_RATIO_PHONE_EMPLOYED[is.infinite(application$Add_RATIO_PHONE_EMPLOYED)] = NA # DAYS_EMPLOYED = 0 (self-employed) generates -Inf
   return(application)
 }
