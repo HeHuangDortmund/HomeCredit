8147803de87b33f46f86c64a7986a1961f10a906
Win7Adm <fang.duan.hu@gmail.com>
2018-08-02 19:47:18 +0200

    prediction result 0208

---
+++ predict/0208/predict_lightGBM_0208.R
@@ -0,0 +1,73 @@
+rm(list=ls())
+pkgs <- c("rprojroot","data.table","mlr","ggplot2","lightgbm")
+lapply(pkgs,require,character.only = TRUE)
+requireNamespace("withr")
+root = find_root(is_git_root)
+set.seed(1)
+
+data = fread("../data/full_data.csv")
+data$Add_RATIO_MEAN_PAYMENT_PREV[is.na(data$Add_RATIO_MEAN_PAYMENT_PREV)] = mean(data$Add_RATIO_MEAN_PAYMENT_PREV, na.rm = TRUE)
+
+train = data[data$split == "train",]
+test = data[data$split == "test",]
+
+train$split = NULL
+test$split = NULL
+
+SK_ID_CURR = test$SK_ID_CURR
+train$SK_ID_CURR = NULL
+test$SK_ID_CURR = NULL
+test$TARGET = NULL
+
+const = c("CODE_GENDER.miss",
+          "NAME_INCOME_TYPE.miss",
+          "NAME_EDUCATION_TYPE.miss",
+          "NAME_FAMILY_STATUS.miss",
+          "NAME_HOUSING_TYPE.miss",
+          "REGION_RATING_CLIENT.miss",
+          "REGION_RATING_CLIENT_W_CITY.miss",
+          "WEEKDAY_APPR_PROCESS_START.miss",
+          "ORGANIZATION_TYPE.miss")
+train[,const]= NULL
+test[,const] = NULL
+
+set.seed(1)
+index = sample(c(1,0), nrow(train), replace = TRUE, prob = c(0.9,0.1))
+train$TARGET = as.numeric(train$TARGET)
+label_tr = train$TARGET[index == 1]
+label_va = train$TARGET[index == 0]
+train$TARGET = NULL
+tr = train[index == 1,]
+va = train[index == 0,]
+
+lgb.tr = lgb.Dataset(data.matrix(tr), label = label_tr)
+lgb.va = lgb.Dataset(data.matrix(va), label = label_va)
+
+lgb.params = list(
+  objective = "binary",
+  metric = "auc",
+  learning_rate = 0.01,
+  num_leaves = 63,
+  max_bin = 511,
+  max_depth = 8,
+  min_data_in_leaf = 3,
+  min_sum_hessian_in_leaf = 100,
+  feature_fraction = 0.9
+)
+
+lgb.model = lgb.train(
+  params = lgb.params,
+  data = lgb.tr,
+  valids = list(val = lgb.va),
+  num_threads = 4, # number of real CPU cores
+  nrounds = 5000,
+  early_stopping_rounds = 3000,
+  eval_freq = 100
+)
+
+imp = lgb.importance(lgb.model, percentage = TRUE)
+lgb.plot.importance(imp, top_n = 100)
+pred = predict(lgb.model, data = data.matrix(test), n = lgb.model$best_iter)
+options(scipen = 3)
+prediction = data.frame(SK_ID_CURR = SK_ID_CURR, TARGET = pred)
+write.csv(prediction, file.path(root,"predict", "submisssions0208_1.csv"), row.names = FALSE)
